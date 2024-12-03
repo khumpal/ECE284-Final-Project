52883d5191531399bcd69d1a9788f9bde1bc8054
Baimeedi <nbaimeedi@ieng6-ece-04.ucsd.edu>
2024-12-03 10:37:12 -0800

    core_tb.v Initial Version

--- Project/core_tb.v
+++ Project/core_tb.v
@@ -15,7 +15,7 @@ parameter len_nij = 36;
 reg clk = 0;
 reg reset = 1;
 
-wire [34:0] inst_q; 
+wire [33:0] inst_q; 
 
 reg [1:0]  inst_w_q = 0; 
 reg [bw*row-1:0] D_xmem_q = 0;
@@ -40,8 +40,6 @@ reg execute_q = 0;
 reg load_q = 0;
 reg acc_q = 0;
 reg acc = 0;
-reg relu_q=0;
-reg relu =0;
 
 reg [1:0]  inst_w; 
 reg [bw*row-1:0] D_xmem;
@@ -66,9 +64,11 @@ integer acc_file, acc_scan_file ; // file_handler
 integer out_file, out_scan_file ; // file_handler
 integer captured_data; 
 integer t, i, j, k, kij;
+integer u,v,w;
 integer error;
 
-assign inst_q[34] = relu_q;
+integer psum_out;
+
 assign inst_q[33] = acc_q;
 assign inst_q[32] = CEN_pmem_q;
 assign inst_q[31] = WEN_pmem_q;
@@ -87,7 +87,7 @@ assign inst_q[0]   = load_q;
 
 core  #(.bw(bw), .col(col), .row(row)) core_instance (
 	.clk(clk), 
-    .inst(inst_q), // should take inst[33]
+	.inst(inst_q),
 	.ofifo_valid(ofifo_valid),
         .D_xmem(D_xmem_q), 
         .sfp_out(sfp_out), 
@@ -136,7 +136,10 @@ initial begin
 
   /////// Activation data writing to memory ///////
   for (t=0; t<len_nij; t=t+1) begin  
-    #0.5 clk = 1'b0;  x_scan_file = $fscanf(x_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0; if (t>0) A_xmem = A_xmem + 1;
+    #0.5 clk = 1'b0;  x_scan_file = $fscanf(x_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0;
+    //$display("Activation File %s",x_file); 
+    $display("Activation data (to memory): %32b",D_xmem);
+    if (t>0) A_xmem = A_xmem + 1;
     #0.5 clk = 1'b1;   
   end
 
@@ -188,10 +191,13 @@ initial begin
 
     /////// Kernel data writing to memory ///////
 
-    A_xmem = 11'b10000000000;
+    A_xmem = 11'b1000_0000_000;
 
     for (t=0; t<col; t=t+1) begin  
-      #0.5 clk = 1'b0;  w_scan_file = $fscanf(w_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0; if (t>0) A_xmem = A_xmem + 1; 
+      #0.5 clk = 1'b0;  w_scan_file = $fscanf(w_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0; 
+      $display("Kernel data (to memory): %32b",D_xmem);
+      $display("Kernel Address (activation memory): %11b",A_xmem);
+      if (t>0) A_xmem = A_xmem + 1; 
       #0.5 clk = 1'b1;  
     end
 
@@ -202,13 +208,46 @@ initial begin
 
 
     /////// Kernel data writing to L0 ///////
+    A_xmem = 11'b1000_0000_000;
+
+    for (u=0; u<col; u=u+1) begin
+	    #0.5 clk = 1'b0;
+	    WEN_xmem = 1; CEN_xmem = 0;
+	    l0_wr <= 1;
+	    $display("Kernel data (to memory): %32b",D_xmem);
+	    if (u>0) begin
+		    //l0_wr_q <= 1;
+		    A_xmem = A_xmem + 1;
+	    end
+	    #0.5 clk = 1'b1;
+    end
+
+    #0.5 clk = 1'b0;  WEN_xmem = 1;  CEN_xmem = 1; A_xmem = 0; l0_wr <= 0;
+    #0.5 clk = 1'b1;
 
     /////////////////////////////////////
 
 
 
     /////// Kernel loading to PEs ///////
-    ...
+    for (v=0;v<col;v=v+1) begin
+	    #0.5 clk = 1'b0;
+	    l0_rd <= 1;
+	    //load_q <= 1;
+	    if (v>0) begin
+		    load <= 1;
+	    end
+	    #0.5 clk = 1'b1;
+    end
+    
+    #0.5 clk = 1'b0;
+    load <= 1; //for reading the last weight into the PE
+    l0_rd <= 0; //to stop reading from L0
+    #0.5 clk = 1'b1;
+
+
+
+
     /////////////////////////////////////
   
 
@@ -227,21 +266,70 @@ initial begin
 
 
     /////// Activation data writing to L0 ///////
-    ...
+    A_xmem = 11'b0000_0000_000;
+
+    for (u=0; u<col; u=u+1) begin
+	    #0.5 clk = 1'b0;
+	    WEN_xmem = 1; CEN_xmem = 0;
+	    l0_wr <= 1;
+	    if (u>0) begin
+		    //l0_wr_q <= 1;
+		    A_xmem = A_xmem + 1;
+	    end
+	    #0.5 clk = 1'b1;
+    end
+
+    #0.5 clk = 1'b0;  WEN_xmem = 1;  CEN_xmem = 1; A_xmem = 0;
+    #0.5 clk = 1'b1;
     /////////////////////////////////////
 
 
 
     /////// Execution ///////
-    ...
+    for (v=0;v<col;v=v+1) begin
+	    #0.5 clk = 1'b0;
+	    l0_rd <= 1;
+	    load <= 0;
+	    //execute_q <= 1;
+	    if (v>0) begin
+		    execute <= 1;
+	    end
+	    #0.5 clk = 1'b1;
+    end
+    
+    #0.5 clk = 1'b0; l0_rd <= 1; //to stop reading from L0
+    execute <= 1; //to send the execute instruction for the last input data
+    #0.5 clk = 1'b1;
+    execute <= 0;
     /////////////////////////////////////
 
+
+    for (i=0; i<10 ; i=i+1) begin
+      #0.5 clk = 1'b0;
+      #0.5 clk = 1'b1;  
+    end
 
 
     //////// OFIFO READ ////////
     // Ideally, OFIFO should be read while execution, but we have enough ofifo
     // depth so we can fetch out after execution.
-    ...
+
+    A_pmem = 11'b0000_0000_000;
+
+    for (w=0;w<col;w=w+1) begin
+	    #0.5 clk = 1'b0;
+	    ofifo_rd <= 1;
+	    CEN_pmem <= 1'b0;
+	    WEN_pmem <= 1'b0;
+	    if (w>0) begin
+		    A_pmem = A_pmem + 1;
+	    end
+	    #0.5 clk = 1'b1;
+    end
+
+    #0.5 clk = 1'b0;  WEN_pmem = 1;  CEN_pmem = 1; A_pmem = 0; ofifo_rd <= 0;
+    #0.5 clk = 1'b1;
+
     /////////////////////////////////////
 
 
@@ -258,6 +346,12 @@ initial begin
 
   error = 0;
 
+  acc_file = $fopen("psum_2.txt", "r");  
+
+  // Following three lines are to remove the first three comment lines of the file
+  acc_scan_file = $fscanf(acc_file,"%s", psum_out); 
+  acc_scan_file = $fscanf(acc_file,"%s", psum_out); 
+  acc_scan_file = $fscanf(acc_file,"%s", psum_out);
 
 
   $display("############ Verification Start during accumulation #############"); 
@@ -329,7 +423,6 @@ always @ (posedge clk) begin
    A_xmem_q   <= A_xmem;
    ofifo_rd_q <= ofifo_rd;
    acc_q      <= acc;
-   relu_q     <=relu;
    ififo_wr_q <= ififo_wr;
    ififo_rd_q <= ififo_rd;
    l0_rd_q    <= l0_rd;
